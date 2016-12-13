c3124aecd6737e1aec466bfcc14d3d43cbf86e31
Eric Youngson <eric@successionecological.com>
2016-12-12 18:25:07 -0800

    Added minimal Bokeh template from tutorial--it works\!

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+    plot = figure(tools="pan,wheel_zoom,box_zoom,reset",
+              title='Data from Quandle WIKI set',
+              x_axis_label='date',
+              x_axis_type='datetime')
+
+    script, div = components(plot)
+    return render_template('index.html', script=script, div=div)
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507,debug=True)
